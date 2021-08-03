d01df93e597bfba78e4f2814fea61f8a885b45e1
Drazi Crendraven <drazisil@users.noreply.github.com>
2021-08-02 21:22:53 -0400

    initial config

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended","prettier"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+}
