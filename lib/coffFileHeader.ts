26bc514c152b5538407ac64b89d0707d02a48c7e
Drazi Crendraven <drazisil@users.noreply.github.com>
2021-08-05 19:53:35 -0400

    saving

--- lib/coffFileHeader.ts
+++ lib/coffFileHeader.ts
@@ -1,12 +1,28 @@
 export interface CoffFileHeaderJson {
     length: number,
     machine: string | undefined
+    numberOfSections: number | undefined
+    timeDateStamp: number | undefined
+    pointerToSymbolTable: number | undefined
+    numberOfSymbols: number | undefined
+    sizeOfOptionalHeader: number | undefined
+    characteristics: string | undefined
+    
   }
   
   export class CoffFileHeader {
     private _buffer: Buffer;
     private _isValid = false;
+    // https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#machine-types
     private _machine: number | undefined
+    private _numberOfSections: number | undefined
+    private _timeDateStamp: number | undefined
+    private _pointerToSymbolTable: number | undefined
+    private _numberOfSymbols: number | undefined
+    // https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#optional-header-image-only
+    private _sizeOfOptionalHeader: number | undefined
+    // https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#characteristics
+    private _characteristics: number | undefined
   
     constructor(inputBuffer: Buffer) {
       this._buffer = inputBuffer;
@@ -15,6 +31,18 @@ export interface CoffFileHeaderJson {
       }
   
       this._machine = this._buffer.readInt16LE(0)
+
+      this._numberOfSections = this._buffer.readInt16LE(2)
+
+      this._timeDateStamp = this._buffer.readInt32LE(4)
+
+      this._pointerToSymbolTable = this._buffer.readInt32LE(8)
+
+      this._numberOfSymbols = this._buffer.readInt32LE(12)
+
+      this._sizeOfOptionalHeader = this._buffer.readInt16LE(16)
+
+      this._characteristics = this._buffer.readInt16LE(18)
   
       this._isValid = true
     }
@@ -30,7 +58,13 @@ export interface CoffFileHeaderJson {
     public toJSON(): CoffFileHeaderJson {
       return {
         length: this._buffer.byteLength,
-        machine: (this._machine || 0).toString(16)
+        machine: (this._machine || 0).toString(16),
+        numberOfSections: this._numberOfSections,
+        timeDateStamp: this._timeDateStamp,
+        pointerToSymbolTable: this._pointerToSymbolTable,
+        numberOfSymbols: this._numberOfSymbols,
+        sizeOfOptionalHeader: this._sizeOfOptionalHeader,
+        characteristics: (this._characteristics || 0).toString(16),
       }
     }
   }
